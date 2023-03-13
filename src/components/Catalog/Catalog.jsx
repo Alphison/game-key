d23cd52ef3389f3a726b88b0255c4c5437ebc90d
Alphison <alfismusin13@gamil.com>
2023-03-13 10:32:56 +0300

    fsdfs

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -36,6 +36,38 @@ export const tovars = [
         img: 'https://3dnews.ru/assets/external/illustrations/2022/11/12/1077269/Atomic-Heart.jpg',
         price: 787878,
         sale: 50
+    },
+    {
+        id: 5,
+        name: 'game1',
+        text: 'dsakfjabnshjkfhsakdfashdfsajkdfsajkfdjsadgfahjsdfalsdgflkasgdkdfokiawsguefoakie',
+        img: 'https://webmg.ru/wp-content/uploads/2022/06/i-80-1.jpeg',
+        price: 123123,
+        sale: 0
+    },
+    {
+        id: 6,
+        name: 'game2',
+        text: 'dsakfjabnshjkfhsakdfashdfsajkdfsajkfdjsadgfahjsdfalsdgflkasgdkdfokiawsguefoakie',
+        img: 'https://pic.rutubelist.ru/video/52/1d/521da12f59fc15dbdc7949f3aea68f5e.jpg',
+        price: 765567,
+        sale: 0
+    },
+    {
+        id: 7,
+        name: 'game3',
+        text: 'dsakfjabnshjkfhsakdfashdfsajkdfsajkfdjsadgfahjsdfalsdgflkasgdkdfokiawsguefoakie',
+        img: 'https://cdn.ananasposter.ru/image/cache/catalog/poster/games/83/10708-1000x830.jpg',
+        price: 567567,
+        sale: 50
+    },
+    {
+        id: 8,
+        name: 'aaaaaaaaaa',
+        text: 'dsakfjabnshjkfhsakdfashdfsajkdfsajkfdjsadgfahjsdfalsdgflkasgdkdfokiawsguefoakie',
+        img: 'https://i.pinimg.com/originals/4d/4d/47/4d4d473a01857cc346f747a158b851a2.jpg',
+        price: 787878,
+        sale: 50
     }
 ]
 
